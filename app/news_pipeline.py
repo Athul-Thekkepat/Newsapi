9f1e4e6ee163697d3ae0b51bc69e3c1685d179c0
Athul-Thekkepat <tpnandu8@gmail.com>
2026-05-24 16:30:59 +0530

    Added SQLAlchemy database engine

--- app/news_pipeline.py
+++ app/news_pipeline.py
@@ -7,7 +7,7 @@ import os
 
 load_dotenv()
 
-# Integrated News API structure
+# Integrated News API 
 API_KEY = os.getenv("NEWS_API_KEY")
 
 url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY}"
